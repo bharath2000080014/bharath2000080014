d7b6da2960ba2beb4f522674d29613e1c03aa045
bharath2000080014 <88427741+bharath2000080014@users.noreply.github.com>
2021-09-20 16:20:17 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,23 @@
+package com.Kluniversity;
+
+public class Datatypes {
+    public static void main(String[]args){
+        byte age = 19;
+        int phone = 901433989;
+        long phone2=9014339897L;
+        float pi=3.12F;
+        char letter='M';
+        boolean isAdult=true;
+        System.out.println(age);
+        System.out.println(phone);
+        System.out.println(phone2);
+        System.out.println(pi);
+        System.out.println(letter);
+        System.out.println(isAdult);
+        //Non-primitive types
+        String name="Bharath";
+        System.out.println(name.length());
+
+
+    }
+}
