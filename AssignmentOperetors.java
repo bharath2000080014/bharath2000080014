e70fac9d65f363911249b3a41c037e2bff0cdfb0
bharath2000080014 <88427741+bharath2000080014@users.noreply.github.com>
2021-09-20 16:09:05 +0530

    Create AssignmentOperetors.java

---
+++ AssignmentOperetors.java
@@ -0,0 +1,10 @@
+package com.Kluniversity;
+
+public class AssignmentOperetors {
+    public static void main(String[]args){
+        int num=1;
+        num++;
+        System.out.println(num);
+
+    }
+}
