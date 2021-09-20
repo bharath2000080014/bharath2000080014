c98bc00587736f1902aa2b5d96997e62dbb2efff
bharath2000080014 <88427741+bharath2000080014@users.noreply.github.com>
2021-09-20 16:45:35 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,15 @@
+package com.Kluniversity;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        String name="Bharath";
+        int age=99;
+        String neighbour="Babbu";
+        String friend=name;
+        System.out.println(name);
+        System.out.println(neighbour);
+        System.out.println(age);
+        System.out.println(friend+neighbour);
+    }
+}
