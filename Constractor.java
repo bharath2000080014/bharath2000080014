fa9e051b261ee22e102fc2a26a02bf215db5df58
bharath2000080014 <88427741+bharath2000080014@users.noreply.github.com>
2021-10-07 14:07:56 +0530

    Create Constractor.java

---
+++ Constractor.java
@@ -0,0 +1,20 @@
+class A { 
+    int a; 
+ 
+    A() { 
+        int a = 10; 
+        System.out.println("in Class A Constructor"); 
+ 
+    } 
+} 
+ 
+class B extends A { 
+    int b; 
+ 
+    B() { 
+        b = 20; 
+        System.out.println("in class B constructor "); 
+ 
+    } 
+} 
+ 
