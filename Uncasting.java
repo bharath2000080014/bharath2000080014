2656ca6589cdd5b8354d2df86b9c09f3c88fdef6
bharath2000080014 <88427741+bharath2000080014@users.noreply.github.com>
2021-10-09 12:14:34 +0530

    Create Uncasting.java

---
+++ Uncasting.java
@@ -0,0 +1,31 @@
+class A
+{
+    void m1()
+    {
+        System.out.println("inside A's m1 method");
+    }
+}
+class B extends A
+{
+    void m1()
+    {
+        System.out.println("Inside B's m1 method");
+    }
+void disp()
+{
+    System.out.println("Inside disp Method");
+}
+}
+class Main
+{
+    public static void main(String args[])
+    {
+        A a = new A();
+        B b = new B();
+        A ref;
+        ref = a;
+        ref.m1();
+        ref.m1();
+       
+    }
+}
