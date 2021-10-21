a1912dcda285390f75cf67d1558e14b4de97da00
bharath2000080014 <88427741+bharath2000080014@users.noreply.github.com>
2021-10-21 14:40:23 +0530

    Create final keyword.java

---
+++ final keyword.java
@@ -0,0 +1,16 @@
+class GBase {
+public final void display(String s)
+{
+    System.out.println(s);
+} }
+class Sample extends GBase{
+  public void display(String s)
+}
+  System.out.println(s);
+  }
+  public static void main(String args[]){
+      Sample ob =new Sample();
+      ob.display("TRY ME"):
+  }}
+   
+   
